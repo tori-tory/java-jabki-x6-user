408e5ca76d88244c2978343ea975b4ae3f3e2330
tori-tory <molkovaelena@rambler.ru>
2026-01-18 16:32:50 +0300

    init

---
+++ src/main/java/ru/jabki/x6/user/Application.java
@@ -0,0 +1,13 @@
+package ru.jabki.x6.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
